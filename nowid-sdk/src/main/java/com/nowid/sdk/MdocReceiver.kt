47143769027dfc4102748e9d7bd36489c525c3f5
Junbin Deng <junbin.deng@gmail.com>
2025-05-07 14:04:32 +1200

    Add transport strategy interfaces and NFC implementation (#3)

--- nowid-sdk/src/main/java/com/nowid/sdk/MdocReceiver.kt
+++ nowid-sdk/src/main/java/com/nowid/sdk/MdocReceiver.kt
@@ -1,10 +1,8 @@
 package com.nowid.sdk
 
 import android.content.Intent
-import android.nfc.NdefMessage
-import android.nfc.NfcAdapter
-import android.os.Build
 import com.nowid.sdk.cose.message.CoseDispatcher
+import com.nowid.sdk.transport.NfcTransportStrategy
 import java.security.PublicKey
 
 class MdocReceiver {
@@ -14,23 +12,8 @@ class MdocReceiver {
         onResult: (String) -> Unit,
         onError: (String) -> Unit
     ) {
-        // Check if the intent is for NFC
-        if (intent.action != NfcAdapter.ACTION_NDEF_DISCOVERED) {
-            onError("Not NFC action")
-            return
-        }
-
-        // Decode the NFC Data Exchange Format (NDEF) message
-        val rawMessages = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES, NdefMessage::class.java)
-        } else {
-            intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
-        }
-        val ndefMessage = (rawMessages?.firstOrNull() as? NdefMessage) ?: return
-
         try {
-            // Get payload from the first record in the first NDEF message
-            val payload = ndefMessage.records.first().payload
+            val payload = NfcTransportStrategy(intent).extractPayload().getOrThrow()
 
             val result = CoseDispatcher.dispatch(payload, publicKey)
 
