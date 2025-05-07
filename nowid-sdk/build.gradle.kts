a18cf93bd522826ff2d54b9e6c9410a874658818
Junbin Deng <junbin.deng@gmail.com>
2025-05-08 09:38:07 +1200

    Prepare module for publishing

--- nowid-sdk/build.gradle.kts
+++ nowid-sdk/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
@@ -8,11 +8,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.nowid.sdk"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
